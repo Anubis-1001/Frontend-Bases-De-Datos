11f2285460641b8a66d4806c98993acc4343a430
Anubis <correaanubis@gmail.com>
2024-04-28 19:09:16 -0500

    se agrega la vista de recuperar contraseña

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { SignInComponent } from './views/sign-in/sign-in.component';
 import { DefaultViewComponent } from './views/default-view/default-view.component';
 import { HomeComponent } from './views/home/home.component';
 import { ExamsComponent } from './views/exams/exams.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 
 export const routes: Routes = [
     { path: 'sign-in', component: SignInComponent },
     { path: 'default-view', component: DefaultViewComponent },
     { path: 'home', component: HomeComponent},
-    { path: 'exams', component: ExamsComponent}
+    { path: 'exams', component: ExamsComponent},
+    { path: 'forgot-password', component: ForgotPasswordComponent},
 ];
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { routes } from '../../app.routes';
+import { RouterModule, Routes } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  @ViewChild('passwordInput') passwordInput!: ElementRef;
 
+  togglePassword() {
+    const inputType = this.passwordInput.nativeElement.type;
+    this.passwordInput.nativeElement.type = inputType === 'password' ? 'text' : 'password';
+  }
 }
