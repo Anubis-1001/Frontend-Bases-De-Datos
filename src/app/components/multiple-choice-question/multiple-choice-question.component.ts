c719983d1812df78adb79286b7d41edbb187b771
Alejandro Arias <josea.ariasp@uqvirtual.edu.co>
2024-05-17 14:31:44 -0500

    (UPDATE) Finisih multiple choice

--- src/app/components/multiple-choice-question/multiple-choice-question.component.ts
+++ src/app/components/multiple-choice-question/multiple-choice-question.component.ts
@@ -1,18 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, QueryList, ViewChildren } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { PreguntaMultipleComponent } from "../pregunta-multiple/pregunta-multiple.component";
+import { OpcionRespuesta } from '../../dtos/OpcionRespuesta';
 
 @Component({
-  selector: 'app-multiple-choice-question',
-  standalone: true,
-  imports: [FormsModule],
-  templateUrl: './multiple-choice-question.component.html',
-  styleUrl: './multiple-choice-question.component.css'
+    selector: 'app-multiple-choice-question',
+    standalone: true,
+    templateUrl: './multiple-choice-question.component.html',
+    styleUrl: './multiple-choice-question.component.css',
+    imports: [FormsModule, PreguntaMultipleComponent]
 })
 export class MultipleChoiceQuestionComponent {
   
     pregunta!: string;
     options!: string[];
     answer!: string;
+    opciones: OpcionRespuesta[] = [
+        { valor: 'A', seleccionado: false },
+        { valor: 'B', seleccionado: false },
+        { valor: 'C', seleccionado: false },
+        { valor: 'D', seleccionado: false }
+      ];
+    seleccionada: number = -1;
+    @ViewChildren(PreguntaMultipleComponent) childrenComponents!: QueryList<PreguntaMultipleComponent>;
     constructor() { }
   
     ngOnInit(): void {
@@ -21,5 +31,11 @@ export class MultipleChoiceQuestionComponent {
   
     onSubmit() {
   
+    }
+
+    seleccionarOpcion(index: String): void {
+      this.opciones.forEach(opcion => {
+        opcion.seleccionado = opcion.valor === index;
+      });
     }
 }
---
+++ src/app/components/pregunta-multiple/pregunta-multiple.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-pregunta-multiple',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './pregunta-multiple.component.html',
+  styleUrl: './pregunta-multiple.component.css'
+})
+export class PreguntaMultipleComponent {
+  
+
+  @Input() punto!: string;
+
+
+  seleccionar(seleccionada:String): void {
+    console.log('seleccionada', seleccionada);
+  }
+
+}
