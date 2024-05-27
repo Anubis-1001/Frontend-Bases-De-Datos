c936dda370f6e355f538f2dca327b68bcf392586
Alejandro Arias <josea.ariasp@uqvirtual.edu.co>
2024-05-27 10:47:20 -0500

    (UPDATE) Se muestra info examen

--- src/app/components/pending-exam-item/pending-exam-item.component.ts
+++ src/app/components/pending-exam-item/pending-exam-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { ExamDue } from '../../dtos/Exam';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pending-exam-item',
@@ -10,5 +11,17 @@ import { ExamDue } from '../../dtos/Exam';
 })
 export class PendingExamItemComponent {
 
-  @Input() exam?: ExamDue;
+
+  @Input() exam!: ExamDue;
+
+  constructor(private router: Router) {
+
+  }
+
+  presentarExamen() {
+    
+    this.router.navigate(['/presentar-examen', this.exam]);
+
+
+  }
 }
